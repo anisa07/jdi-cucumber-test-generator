2775db95e079c779a5dcb87017930bf2b1de9237
Anatolii_Syunyaev <Anatolii_Syunyaev@epam.com>
2018-02-09 11:52:33 +0300

    TokenMalformedException javadoc added

--- src/main/java/com/epam/test_generator/services/exceptions/TokenMalformedException.java
+++ src/main/java/com/epam/test_generator/services/exceptions/TokenMalformedException.java
@@ -1,9 +1,15 @@
 package com.epam.test_generator.services.exceptions;
 
+import com.epam.test_generator.controllers.GlobalExceptionController;
+import com.epam.test_generator.config.security.JwtAuthenticationProvider;
 import org.springframework.security.core.AuthenticationException;
 
-public class TokenMalformedException extends
-    AuthenticationException {
+/**
+ * Exception that can be thrown when {@link JwtAuthenticationProvider} API user tries to retrieve user
+ * with wrong authentication token. {@link GlobalExceptionController} catches
+ * this exception.
+ */
+public class TokenMalformedException extends AuthenticationException {
 
     public TokenMalformedException(String s) {
         super(s);
