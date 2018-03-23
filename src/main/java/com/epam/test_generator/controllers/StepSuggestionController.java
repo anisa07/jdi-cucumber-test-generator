92fa865a354723a7cd36375d714812da1a8e9a5f
Iurii Ivanov <iurii.ivanov@thomsonreuters.com>
2018-03-23 14:17:56 +0300

    test

--- src/main/java/com/epam/test_generator/controllers/StepSuggestionController.java
+++ src/main/java/com/epam/test_generator/controllers/StepSuggestionController.java
@@ -108,41 +108,41 @@ public class StepSuggestionController {
         @ApiResponse(code = 400, message = "Invalid input")
     })
     @ApiImplicitParams({
-        @ApiImplicitParam(name = "stepSuggestionCreateDTO", value = "Added step suggestion object",
-            required = true, dataType = "StepSuggestionCreateDTO", paramType = "body"),
-        @ApiImplicitParam(name = "Authorization", value = "add here your token", paramType = "header", dataType = "string", required = true)
+            @ApiImplicitParam(name = "stepSuggestionCreateDTO", value = "Added step suggestion object",
+                    required = true, dataType = "StepSuggestionCreateDTO", paramType = "body"),
+            @ApiImplicitParam(name = "Authorization", value = "add here your token", paramType = "header", dataType = "string", required = true)
     })
     @Secured({"ROLE_ADMIN", "ROLE_TEST_ENGINEER", "ROLE_TEST_LEAD"})
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(value = "/stepSuggestions", method = RequestMethod.POST,
         consumes = "application/json", produces = "application/json")
     public ResponseEntity<Long> addStepSuggestion(
-        @RequestBody @Valid StepSuggestionCreateDTO stepSuggestionCreateDTO) {
+            @RequestBody @Valid StepSuggestionCreateDTO stepSuggestionCreateDTO) {
 
         return new ResponseEntity<>(stepSuggestionService.addStepSuggestion(stepSuggestionCreateDTO),
-            HttpStatus.OK);
+                HttpStatus.OK);
     }
 
     @ApiOperation(value = "Update step suggestion by id", nickname = "updateStepSuggestion")
     @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "OK"),
-        @ApiResponse(code = 400, message = "Invalid input"),
-        @ApiResponse(code = 404, message = "StepSuggestion not found"),
-        @ApiResponse(code = 409, message = "StepSuggestion already modified")
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Invalid input"),
+            @ApiResponse(code = 404, message = "StepSuggestion not found"),
+            @ApiResponse(code = 409, message = "StepSuggestion already modified")
     })
     @ApiImplicitParams({
-        @ApiImplicitParam(name = "stepSuggestionId", value = "ID of step suggestion to update",
-            required = true, dataType = "long", paramType = "path"),
-        @ApiImplicitParam(name = "stepSuggestionUpdateDTO", value = "Updated step suggestion object",
-            required = true, dataType = "StepSuggestionUpdateDTO", paramType = "body"),
-        @ApiImplicitParam(name = "Authorization", value = "add here your token",
-            paramType = "header", dataType = "string", required = true)
+            @ApiImplicitParam(name = "stepSuggestionId", value = "ID of step suggestion to update",
+                    required = true, dataType = "long", paramType = "path"),
+            @ApiImplicitParam(name = "stepSuggestionUpdateDTO", value = "Updated step suggestion object",
+                    required = true, dataType = "StepSuggestionUpdateDTO", paramType = "body"),
+            @ApiImplicitParam(name = "Authorization", value = "add here your token",
+                    paramType = "header", dataType = "string", required = true)
     })
     @Secured({"ROLE_ADMIN", "ROLE_TEST_ENGINEER", "ROLE_TEST_LEAD"})
     @RequestMapping(value = "/stepSuggestions/{stepSuggestionId}", method = RequestMethod.PUT, produces = "application/json")
     public ResponseEntity<Void> updateStepSuggestion(
-        @PathVariable("stepSuggestionId") long stepSuggestionId,
-        @RequestBody @Valid StepSuggestionUpdateDTO stepSuggestionUpdateDTO) {
+            @PathVariable("stepSuggestionId") long stepSuggestionId,
+            @RequestBody @Valid StepSuggestionUpdateDTO stepSuggestionUpdateDTO) {
         stepSuggestionService.updateStepSuggestion(stepSuggestionId, stepSuggestionUpdateDTO);
 
         return new ResponseEntity<>(HttpStatus.OK);
